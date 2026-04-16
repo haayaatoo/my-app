7a6e125fdf05a5f2400e351a3a220a6395777106
wada <a-wada@1dr.co.jp>
2026-04-17 01:13:35 +0900

    test: green blue deploy check

--- prodia-backend/prodia_backend/prodia_backend/settings.py
+++ prodia-backend/prodia_backend/prodia_backend/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # --- ファイルアップロード用設定 ---
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
