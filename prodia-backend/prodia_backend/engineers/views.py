b12d263b5e741f12bb7bae4859af9eb148b1d6e8
wada <a-wada@1dr.co.jp>
2026-04-17 10:17:52 +0900

    test: break health02

--- prodia-backend/prodia_backend/engineers/views.py
+++ prodia-backend/prodia_backend/engineers/views.py
@@ -32,7 +32,7 @@ from .serializers import PartnerEngineerSerializer, TeleapoRecordSerializer, Mon
 from django.http import JsonResponse
 
 def health_check(request):
-    return JsonResponse({"status": "ok"})
+    return JsonResponse({"status": "ng"}, status=500)
 
 class EngineerViewSet(viewsets.ModelViewSet):
     queryset = Engineer.objects.all()
